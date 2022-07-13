f3eb4091d46953a9fbb67721c5c5d97d80a1d5fe
levYyyyy <107996072+levYyyyy@users.noreply.github.com>
2022-07-13 20:40:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,3 @@ include("App/Model/SomeModel.php");
 
 // We dont want to use static methods, so we r giving array for call_user_func
 Router::set("/", [SomeController::class, 'index']);
-
-$host = getenv('DB_HOST');
-echo($host);
